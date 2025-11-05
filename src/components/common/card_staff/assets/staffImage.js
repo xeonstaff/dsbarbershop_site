de33ae974679c4200022b33ac52d6c212c15be33
Joel <joelmounts@gmail.com>
2025-11-05 14:27:09 -0500

    updates to staff

---
+++ src/components/common/card_staff/assets/staffImage.js
@@ -0,0 +1,79 @@
+import guli from './guli.webp';
+import logan from './logan.webp';
+import moody from './moody.webp';
+import russ from './russ.webp';
+import sonya from './sonya.webp';
+import chase from './chase.webp';
+import tyler from './tyler.webp';
+
+import guli2 from './guli2.webp';
+import logan2 from './logan2.webp';
+import moody2 from './moody2.webp';
+import russ2 from './russ2.webp';
+import sonya2 from './sonya2.webp';
+import chase2 from './chase2.webp';
+import tyler2 from './tyler2.webp';
+
+import ryan from './ryan.webp';
+import j from './j.webp';
+import trey from './trey.webp';
+import ali_flats from './ali_flats.webp';
+import reign from './reign.webp';
+
+import ryan2 from './ryan2.webp';
+import j2 from './j2.webp';
+import trey2 from './trey2.webp';
+import reign2 from './reign2.webp';
+
+const staffImages = {
+  theflats: {
+    j: {
+      primary: j,
+      secondary: j2,
+    },
+    ryan: {
+      primary: ryan,
+      secondary: ryan2,
+    },
+    trey: {
+      primary: trey,
+      secondary: trey2,
+    },
+    reign: {
+      primary: reign,
+      secondary: reign2,
+    },
+  },
+  thegreene: {
+    guli: {
+      primary: guli,
+      secondary: guli2,
+    },
+    logan: {
+      primary: logan,
+      secondary: logan2,
+    },
+    moody: {
+      primary: moody,
+      secondary: moody2,
+    },
+    russ: {
+      primary: russ,
+      secondary: russ2,
+    },
+    chase: {
+      primary: chase,
+      secondary: chase2,
+    },
+    tyler: {
+      primary: tyler2,
+      secondary: tyler,
+    },
+    sonya: {
+      primary: sonya,
+      secondary: sonya2,
+    },
+  },
+};
+
+export { staffImages };
---
+++ src/content/locations/thegreene.md
@@ -0,0 +1,26 @@
+---
+thePlace: "The Greene"
+location: "Beavercreek , OH"
+address: "4411 Walnut St. Beavercreek, OH 45440"
+phone: "(937)-702-9370"
+email: "dsbarbershop1@gmail.com"
+pagePath: "locations/thegreene"
+bookPath: 
+  {
+    "store":"https://reports.appheaven.us/online/cbb.php?merchantid=R0YG283WBBCC1",
+    "Guli":"https://greene.fivease.com/online/bookme?guli",
+    "Sonya":"https://greene.fivease.com/online/bookme?sonya",
+    "Logan":"https://greene.fivease.com/online/bookme?logan",
+    "Moody":"https://greene.fivease.com/online/bookme?moody",
+    "Russ":"https://greene.fivease.com/online/bookme?russ",
+    "Chase":"https://greene.fivease.com/online/bookme?chase",
+    "Tyler":"https://greene.fivease.com/online/bookme?tyler"
+  }
+staff: ["Guli", "Sonya", "Logan", "Moody", "Russ", "Chase", "Tyler"]
+hours:
+  [
+    { day: "Monday to Friday", time: "9am-6pm" },
+    { day: "Saturday", time: "9am-5pm" },
+    { day: "Sunday", time: "10am - 4pm" },
+  ]
+---

35c625e0724c47f675c38541dce3f7f698da08b5
Joel <joelmounts@gmail.com>
2026-03-12 14:20:31 -0400

    image updates; removed chase

--- node_modules/@astrojs/ts-plugin/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const createLanguageServicePlugin_js_1 = require("@volar/typescript/lib/quickstart/createLanguageServicePlugin.js");
-const language_js_1 = require("./language.js");
-module.exports = (0, createLanguageServicePlugin_js_1.createLanguageServicePlugin)(() => [(0, language_js_1.getLanguageModule)()]);
